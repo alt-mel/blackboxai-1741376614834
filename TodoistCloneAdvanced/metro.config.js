eebdee0e815c0a5fd2feb9374efefb8f206bf711
Melissa Dennis <melissadennis120@gmail.com>
2025-03-07 17:43:30 -0400

    Add TodoistCloneAdvanced/metro.config.js 1741383809842

--- TodoistCloneAdvanced/metro.config.js
+++ TodoistCloneAdvanced/metro.config.js
@@ -1,11 +1,15 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
 
-/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-// Add support for importing SVG files
-config.resolver.assetExts.push('svg');
+// Add any custom Metro configuration here
+config.resolver.sourceExts = [
+  ...config.resolver.sourceExts,
+  'mjs',
+  'cjs',
+];
+
+// Support for SVG files if needed
 config.transformer.babelTransformerPath = require.resolve('react-native-svg-transformer');
 
 module.exports = config;

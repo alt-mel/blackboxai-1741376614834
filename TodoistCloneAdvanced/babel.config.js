7f4616c092906745644d6018306ebc4e01a77e94
Melissa Dennis <melissadennis120@gmail.com>
2025-03-08 08:21:42 -0400

    Add TodoistCloneAdvanced/babel.config.js 1741436502434

--- TodoistCloneAdvanced/babel.config.js
+++ TodoistCloneAdvanced/babel.config.js
@@ -1,39 +1,28 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      'react-native-reanimated/plugin',
       [
         'module-resolver',
         {
           root: ['./src'],
-          extensions: [
-            '.ios.ts',
-            '.android.ts',
-            '.ts',
-            '.ios.tsx',
-            '.android.tsx',
-            '.tsx',
-            '.jsx',
-            '.js',
-            '.json',
-          ],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
           alias: {
-            '@': './src',
             '@components': './src/components',
             '@screens': './src/screens',
             '@navigation': './src/navigation',
             '@contexts': './src/contexts',
             '@hooks': './src/hooks',
-            '@services': './src/services',
             '@utils': './src/utils',
             '@types': './src/types',
-            '@config': './src/config',
             '@theme': './src/theme',
+            '@config': './src/config',
+            '@services': './src/services',
           },
         },
       ],
+      'react-native-reanimated/plugin',
     ],
   };
 };
